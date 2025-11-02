5803ebdc1bf20336c98ddf8177d75fd260fd768f
Tripleo <tripleo@tripleo.milkyway>
2025-11-01 22:21:15 -0400

    ?

--- src/main/java/tripleo/elijah/Main.java
+++ src/main/java/tripleo/elijah/Main.java
@@ -9,7 +9,6 @@ import tripleo.elijah_fluffy.util.*;
 import java.util.*;
 
 public class Main {
-
 	public static void main(final String[] args) throws Exception {
 		ElavetedRunner elaveted = new ElavetedRunner();
 		elaveted.defaultCompilerController();
