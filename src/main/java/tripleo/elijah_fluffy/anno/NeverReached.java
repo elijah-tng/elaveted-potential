0b3217da8f673245c699e392b7193aaf61ca255e
Tripleo <tripleo@tripleo.milkyway>
2025-11-01 23:25:10 -0400

    shore up

--- src/main/java/tripleo/elijah_fluffy/anno/NeverReached.java
+++ src/main/java/tripleo/elijah_fluffy/anno/NeverReached.java
@@ -1,4 +1,9 @@
 package tripleo.elijah_fluffy.anno;
 
+import tripleo.elijah_fluffy.util.*;
+
 public class NeverReached extends RuntimeException {
+	public NeverReached(){
+		NotImplementedException.raise_stop();
+	}
 }
