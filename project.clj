1e6cf99c338c126fd77918558bf3390610859cf7
tripleo1 <50311859+tripleo1@users.noreply.github.com>
2026-03-14 01:18:12 +0000

    Add junit for gh-ci

--- project.clj
+++ project.clj
@@ -54,7 +54,9 @@
    ;; [org.checkerframework/checker-qual "3.42.0"]
 
    [org.slf4j/slf4j-api "1.7.25"]
-   [org.slf4j/slf4j-simple "1.7.25"]]
+   [org.slf4j/slf4j-simple "1.7.25"]
+   
+   [junit/junit "4.13.2" :scope "test"]]
 
   :source-paths
   ["src/main/clojure"]
